b8537c15e0301e1d6ee0d96c1e5519ec51584be3
Taha Erden <tahaerden@windowslive.com>
2019-08-01 13:40:20 +0300

    Re-implemented portfolio. Fixed webpack config. Fixed bad css.

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,18 @@
+const path = require('path')
 module.exports = {
+  mode: 'none',
+  devServer: {
+    hot: false,
+    inline: false
+  },
   entry: './index.js',
   output: {
     // path: __dirname,
+    path: path.resolve(__dirname, ''),
     filename: './bundle.js'
   },
-  module: 
-  {
-    loaders: 
+  module: {
+    rules:
     [
       // the url-loader uses DataUrls.
       // the file-loader emits files.
@@ -15,25 +21,33 @@ module.exports = {
       // {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file'},
       // {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml'},
 
-      {test: /\.json$/, loader: 'json-loader'},
+      // {test: /\.json$/, loader: 'json-loader'},
       {test: /\.css$/, loader: 'style!css!'},
       // {test: /\.less$/, loader: 'style!raw!css!less'},
-      { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=10000&minetype=application/font-woff" },
-      { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader" }
+      { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'url-loader?limit=10000&minetype=application/font-woff' },
+      { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'file-loader' },
+      {
+        // Only for area calendar
+        test: /\.exec\.js$/,
+        use: [ 'script-loader' ]
+      }
 
       // { test: /\.js/, loader: 'imports?define=>false'}
       // { test   : /\.woff|\.woff2|\.svg|.eot|\.ttf/, loader : 'url?prefix=font/&limit=10000'}
-      ]
-    }
-// Resolve for jquery UI 
+    ]
+  }
+  // node: {
+  //   fs: "empty"
+  // }
+// Resolve for jquery UI
 //     ,
 //   resolve : {
 //     alias: {
 //       // bind version of jquery-ui
-//       "jquery-ui": "jquery-ui/jquery-ui.js",      
+//       "jquery-ui": "jquery-ui/jquery-ui.js",
 //       // bind to modules;
 
 //       modules: path.join(__dirname, "node_modules/")
 //     }}
 
-  };
+}
